e3c443c6ef9908d32581b1c3f01257c97cf61397
Abfackeln <synart21@gmail.com>
2019-08-25 23:48:19 +0300

    Add Form Logic to Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from '../_services';
 
 @Component({ 
     templateUrl: 'login.component.html' 
 })
 
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+    error: string;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        // redirect to home if already logged in
+        if(this.authenticationService.currentUserValue){
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngOnInit(){
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    onSubmit(){
+        this.submitted = true;
+        this.authenticationService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.error = error;
+                    this.loading = false;
+                }
+            );
+
+    }
+}
